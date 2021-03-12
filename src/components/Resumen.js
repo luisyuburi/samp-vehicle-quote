90f4f608666632c90c63bed197aa74ed38dd19c7
Luis Alejandro Yuburi <luisyuburi@outlook.com>
2021-03-12 15:00:51 -0500

    first commit

---
+++ src/components/Resumen.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { primeraMayuscula } from '../helper'
+import PropTypes from 'prop-types'
+
+const ContenedorResumen = styled.div`
+  padding: 1rem;
+  text-align: center;
+  background-color: #00838f;
+  color: #fff;
+  margin-top: 1rem;
+`
+
+const Resumen = ({ datos }) => {
+  // Extraer datos
+
+  const { marca, year, plan } = datos
+
+  if (marca === '' || plan === '' || year === '') {
+    return null
+  }
+
+  return (
+    <ContenedorResumen>
+      <h2>Resumen de cotizacion</h2>
+      <ul>
+        <li>Marca: {primeraMayuscula(marca)}</li>
+        <li>Plan: {primeraMayuscula(plan)}</li>
+        <li>Año: {year}</li>
+      </ul>
+    </ContenedorResumen>
+  )
+}
+Resumen.propTypes = {
+  datos: PropTypes.object.isRequired,
+}
+export default Resumen
